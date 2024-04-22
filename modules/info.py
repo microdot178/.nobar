f8acfded622320b96bac2e58d558115474646cf9
microdot178 <microdot178@tuta.io>
2024-04-22 22:16:03 +0000

    feat(modules): add info panel

--- modules/info.py
+++ modules/info.py
@@ -1,13 +1,12 @@
 from PyQt6.QtWidgets import QHBoxLayout, QLabel
 from PyQt6.QtCore import Qt, QTimer, QDateTime
+import psutil
 from core.panel import Panel
 
 class Info(Panel):
     def __init__(self):
         super(Info, self).__init__()
         self.setWindowTitle('microbar_info')
-
-        # self.setWindowRole('microbar_info')
 
         self.layout = QHBoxLayout()
         self.setLayout(self.layout)
@@ -17,15 +16,18 @@ class Info(Panel):
         self.label.setStyleSheet(self.styleSheet)
         self.layout.addWidget(self.label)
 
-        self.update()
-
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update)
         self.timer.start(1000)
 
     def update(self):
-        print('updated: info')
+        # print('updated: info', self.label.width())
 
+        percentage = psutil.sensors_battery().percent
         time = QDateTime.currentDateTime().toString('hh:mm:ss')
 
-        self.label.setText(time)
+        self.label.setText('{0} {1}'.format(percentage, time))
+
+        self.move(1920 - self.width(), 0)
+        self.show()
+
