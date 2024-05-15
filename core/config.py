f4352ac2f0f1712a47397f7211f4778c963c3cdc
microdot178 <microdot178@tuta.io>
2024-05-15 15:36:05 +0000

    created default configuration file setup

--- core/config.py
+++ core/config.py
@@ -1,5 +1,32 @@
 import tomllib
+import os
 
-with open('config.toml', 'rb') as f:
-    config = tomllib.load(f)
+DEFAULT_CONFIG = """
+[workspaces]
+height = 50
+color = 'gray'
+focused = 'white'
+background = 'black'
+float = 'left'
+
+[info]
+height = 50
+color = 'white'
+background = 'black'
+float = 'right'
+"""
+
+def create_default_config(config_filename):
+    with open(config_filename, 'w') as f:
+        f.write(DEFAULT_CONFIG)
+
+def get_config(config_filename):
+    if not os.path.exists(config_filename):
+        create_default_config(config_filename)
+        print(f"Created default configuration file: {config_filename}")
+
+    with open(config_filename, 'rb') as f:
+        config = tomllib.load(f)
+
+    return config
 
