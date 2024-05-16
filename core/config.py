6bb964e5e5c52184b0bdb6971c812e50dd12680b
microdot178 <microdot178@tuta.io>
2024-05-16 13:04:01 +0000

    updated configuration position reading

--- core/config.py
+++ core/config.py
@@ -7,26 +7,27 @@ height = 50
 color = 'gray'
 focused = 'white'
 background = 'black'
-float = 'left'
+position = [0, 0]
 
 [info]
 height = 50
 color = 'white'
 background = 'black'
-float = 'right'
+position = ['right', 0]
 """
 
+
 def create_default_config(config_filename):
-    with open(config_filename, 'w') as f:
+    with open(config_filename, "w") as f:
         f.write(DEFAULT_CONFIG)
+
 
 def get_config(config_filename):
     if not os.path.exists(config_filename):
         create_default_config(config_filename)
         print(f"Created default configuration file: {config_filename}")
 
-    with open(config_filename, 'rb') as f:
+    with open(config_filename, "rb") as f:
         config = tomllib.load(f)
 
     return config
-
--- core/panel.py
+++ core/panel.py
@@ -1,7 +1,8 @@
 from PyQt6.QtWidgets import QWidget
 from abc import abstractmethod
-from PyQt6.QtGui import QFont, QPalette, QColor
+from PyQt6.QtGui import QGuiApplication, QFont, QPalette, QColor
 from PyQt6.QtCore import Qt
+
 
 class Panel(QWidget):
     def __init__(self, config):
@@ -9,19 +10,35 @@ class Panel(QWidget):
         self.setWindowFlag(Qt.WindowType.ToolTip)
 
         palette = self.palette()
-        palette.setColor(QPalette.ColorRole.Window, QColor(config['background']))
+        palette.setColor(QPalette.ColorRole.Window, QColor(config["background"]))
         self.setPalette(palette)
         # self.setAutoFillBackground(True)
 
         self.font = QFont()
-        self.font.setFamily(config['font'])
-        self.font.setPointSize(config['font-size'])
-
-        self.styleSheet = 'color: {}'.format(config['color'])
-
+        self.styleSheet = "color: {}".format(config["color"])
         self.config = config
+
+        if "font" in config:
+            self.font.setFamily(config["font"])
+
+        if "font-size" in config:
+            self.font.setPointSize(config["font-size"])
+
+        self.show()
+
+    def set_position(self):
+        position = self.config["position"]
+
+        screen = QGuiApplication.primaryScreen().size()
+        width = screen.width()
+        height = screen.height()
+
+        x = width - self.width() if position[0] == "right" else position[0]
+        y = height - self.height() if position[1] == "bottom" else position[1]
+
+        self.move(x, y)
+        self.adjustSize()
 
     @abstractmethod
     def update(self):
         pass
-
