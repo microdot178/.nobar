77d79c928c8bd2e69a826809913f2036b0b81e7e
microdot178 <microdot178@tuta.io>
2025-08-19 21:09:30 +0300

    added install.sh

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# .nobar installation script
+
+set -e
+
+CONFIG_DIR="$HOME/.config/nobar"
+BIN_DIR="$HOME/.local/bin"
+VENV_DIR="$HOME/.virtualenvs/nobar"
+
+mkdir -p "$CONFIG_DIR"
+mkdir -p "$BIN_DIR"
+mkdir -p "$VENV_DIR"
+
+echo "Creating default config..."
+cat >"$CONFIG_DIR/config.toml" <<'EOF'
+[workspaces]
+height = 40
+color = 'gray'
+focused = 'white'
+background = 'black'
+position = [0, 0]
+fade_out = 5000
+
+[info]
+height = 40
+color = 'white'
+background = 'black'
+position = ['right', 0]
+fade_out_on_hover = true
+EOF
+
+echo "Creating executable wrapper..."
+cat >"$BIN_DIR/nobar" <<EOF
+#!/bin/bash
+source $VENV_DIR/bin/activate
+python $PWD/main.py $CONFIG_DIR/config.toml
+EOF
+
+chmod +x "$BIN_DIR/nobar"
+
+echo "Installing Python dependencies..."
+python -m venv $VENV_DIR
+source "$VENV_DIR/bin/activate"
+pip install --upgrade pip
+pip install -r requirements.txt
