1470840864224a3b608026b9e09338f361065c94
microdot178 <microdot178@tuta.io>
2025-09-03 01:46:11 +0300

    add abstract properties to AppABC

--- interfaces/app.py
+++ interfaces/app.py
@@ -1,10 +1,27 @@
 from abc import ABC, abstractmethod
+from typing import Any, List, Optional
 
+import i3ipc
 from i3ipc.aio import Connection
 from i3ipc.events import IpcBaseEvent
 
 
 class AppABC(ABC):
+    @property
+    @abstractmethod
+    def i3(self) -> Optional[Connection]:
+        pass
+
+    @property
+    @abstractmethod
+    def connection(self) -> i3ipc.Connection:
+        pass
+
+    @property
+    @abstractmethod
+    def widgets(self) -> List[Any]:
+        pass
+
     @abstractmethod
     async def start(self) -> None:
         pass
